adf022bf11b66fcd9eb118b8656ecf28f505baff
Kathleen House <kathleenhouse@Macintosh-141.local>
2016-04-15 09:48:47 -0400

    Revert "Merge pull request #54 from BrandenSandahl/master"
    This reverts commit d66e7d1e82e69125d077809325d312d2a0acfdc6, reversing
    changes made to b7e70cc8eb72151aa481392a12a53107c8f53dcb.

--- public/myApp/www/lib/angular-ui-router/api/angular-ui-router.d.ts
+++ public/myApp/www/lib/angular-ui-router/api/angular-ui-router.d.ts
@@ -6,12 +6,12 @@
 declare module ng.ui {
 
     interface IState {
-        vendorName?: string;
+        name?: string;
         template?: string;
         templateUrl?: any; // string || () => string
         templateProvider?: any; // () => string || IPromise<string>
         controller?: any;
-        controllerAs?: string;
+        controllerAs?: string;    
         controllerProvider?: any;
         resolve?: {};
         url?: string;
@@ -29,9 +29,9 @@ declare module ng.ui {
     }
 
     interface IStateProvider extends IServiceProvider {
-        state(vendorName: string, config: IState): IStateProvider;
+        state(name: string, config: IState): IStateProvider;
         state(config: IState): IStateProvider;
-        decorator(vendorName?: string, decorator?: (state: IState, parent: Function) => any): any;
+        decorator(name?: string, decorator?: (state: IState, parent: Function) => any): any;
     }
 
     interface IUrlMatcher {
@@ -118,7 +118,7 @@ declare module ng.ui {
 
     interface IUiViewScrollProvider {
         /*
-         * Reverts back to using the core $anchorScroll service for scrolling
+         * Reverts back to using the core $anchorScroll service for scrolling 
          * based on the url anchor.
          */
         useAnchorScroll(): void;
