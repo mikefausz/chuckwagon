edf06751d00812a9e0ce27bec93819d0090f304c
Michael Fausz <mikefausz@Michaels-MacBook-Pro.local>
2016-04-18 09:28:30 -0400

    Switched to new logos/icons

--- public/myApp/www/favorites/favorites.controllers.js
+++ public/myApp/www/favorites/favorites.controllers.js
@@ -22,8 +22,7 @@ angular
       var marker = new google.maps.Marker({
         position: latLng,
         map: $scope.map,
-        title: 'You are here',
-        icon: 'http://www.euroheat.co.uk/images/you-are-here-icon.png'
+        icon: 'logo-pin-here.png'
       });
 
       marker.setMap($scope.map);
@@ -35,7 +34,7 @@ angular
           var marker = new google.maps.Marker({
             position: truck.location,
             map: $scope.map,
-            icon: 'icon-tutone.png',
+            icon: 'logo-pin-shadow-white-sm.png',
           });
 
           var contentString = "<div><a ng-href='#/tab/list/" + truck.id + "'>" + truck.name + "</a></div>";
@@ -84,7 +83,7 @@ angular
   $scope.hasContent = function() {
     return $scope.truck.location.tweet || $scope.truck.location.imageUrl;
   };
-  
+
     $scope.$on('favorite:added', function () {
       FavoritesService.getFavoriteTrucks().then(function(trucks) {
         $scope.trucks = trucks;
