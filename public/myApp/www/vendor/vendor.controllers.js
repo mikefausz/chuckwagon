b5d77057c82cc36a001dc7271c1082558f27a7a0
Michael Fausz <mikefausz@Michaels-MacBook-Pro.local>
2016-04-25 14:43:01 -0400

    Adjusted formatting for live hosting

--- public/myApp/www/vendor/vendor.controllers.js
+++ public/myApp/www/vendor/vendor.controllers.js
@@ -77,7 +77,6 @@ angular
     })
 
     .controller('VendordashdetailCtrl', function($scope, VendorService, $stateParams){
-<<<<<<< HEAD
       $scope.currentVendor = JSON.parse(localStorage.getItem('currentVendor'));
 
       $scope.hasContent = function() {
@@ -88,17 +87,7 @@ angular
            return false;
          }
        };
-=======
-        $scope.currentVendor = JSON.parse(localStorage.getItem('currentVendor'));
-        $scope.hasContent = function() {
-           if($scope.currentVendor.location) {
-             return $scope.currentVendor.location.tweet || $scope.currentVendor.location.imageUrl;
-           }
-           else {
-             return false;
-           }
-         };
->>>>>>> fe526dbec18a5cba59333b764253be8f2709c521
+
         console.log('IN DASH DETAIL');
         console.log("CURRENT VENDOR " + $scope.currentVendor);
           var mapOptions = {
@@ -112,12 +101,7 @@ angular
           var marker = new google.maps.Marker({
             position: {lat: $scope.currentVendor.location.lat, lng: $scope.currentVendor.location.lng},
             map: $scope.map,
-<<<<<<< HEAD
-            icon: 'logo-pin-shadow-white-sm.png',
-=======
             icon: 'logo-pin-here.png'
-
->>>>>>> fe526dbec18a5cba59333b764253be8f2709c521
           });
 
           marker.setMap($scope.map);
